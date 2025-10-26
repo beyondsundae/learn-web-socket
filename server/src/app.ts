a665b69319b40ddb8bb0fa14746694d4a0f98f36
Thanakrit <thanakrit@futuremakers.co.th>
2025-10-26 14:10:54 +0700

    chore: add simply initialize

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,23 @@
 import express from "express";
 const app = express();
 import dotenv from "dotenv";
+import http from "http";
 import path from "path";
 dotenv.config({ path: path.resolve(__dirname, "../.env.local") });
 
 import WebSocket, { WebSocketServer } from "ws";
+
+/** initiial */
+const serverNode = http.createServer((req, res) => {
+  // Set the response header
+  res.writeHead(200, { "Content-Type": "text/plain" });
+
+  // Send the response body
+  res.end("Server running");
+});
+serverNode.listen(process.env.PORT, () => {
+  console.log(`Server running at http://localhost:${process.env.PORT}/`);
+});
 
 const server = new WebSocketServer({
   port: Number(process.env.SOCKET_PORT) || 8081,
